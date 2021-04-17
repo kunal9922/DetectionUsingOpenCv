b1c533b686ec06a9b630b60a246076fd983665e9
kunal <53283003+kunal9922@users.noreply.github.com>
2021-04-17 12:13:27 +0530

    added test video of hand for the program testing

--- handTrackingMini.py
+++ handTrackingMini.py
@@ -2,7 +2,7 @@ import cv2
 import mediapipe as mp
 import time
 
-cap = cv2.VideoCapture(r"C:\Users\kunal\Videos\handTestVideo.mp4")
+cap = cv2.VideoCapture(r"dataSet\handTestVideo.mp4")
 #import the class  from mediapipe
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
@@ -11,10 +11,10 @@ while True:
       success, img = cap.read()
       imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
       results = hands.process(imgRGB)
-      print(results)
+      print(results.multi_hand_landmarks)
       
       
-      cv2.imshow("Hand", results)
+      #cv2.imshow("Hand", results)
       cv2.waitKey(1)
       
 
