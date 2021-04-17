bcca24f9b34786ff25b5640509079cf325b88fa0
kunal <53283003+kunal9922@users.noreply.github.com>
2021-04-17 12:25:58 +0530

    able to detect multiple hands, detected handlandmarks and draw marks on hand which detected

--- handTrackingMini.py
+++ handTrackingMini.py
@@ -6,15 +6,21 @@ cap = cv2.VideoCapture(r"dataSet\handTestVideo.mp4")
 #import the class  from mediapipe
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils # -----this method allows us to drawlines on hands
 
 while True:
       success, img = cap.read()
       imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-      results = hands.process(imgRGB)
-      print(results.multi_hand_landmarks)
+      results = hands.process(imgRGB)  #           result contains the detected landmarks as dictionary 
+      # print(results.multi_hand_landmarks)
+
+      # --- detected the hand 
+      if results.multi_hand_landmarks:
+            # get the how many hand we detected in an img.
+            for handlms in results.multi_hand_landmarks:
+                   mpDraw.draw_landmarks(img, handlms)
       
-      
-      #cv2.imshow("Hand", results)
+      cv2.imshow("Hand", img)
       cv2.waitKey(1)
       
 
