e8c72694e73dc5a7365b14c915f1ebf743c6826a
kunal <53283003+kunal9922@users.noreply.github.com>
2021-04-17 12:02:32 +0530

    we are just detect the hand nothing doing to show the landmarks

--- handTrackingMini.py
+++ handTrackingMini.py
@@ -3,12 +3,20 @@ import mediapipe as mp
 import time
 
 cap = cv2.VideoCapture(r"C:\Users\kunal\Videos\handTestVideo.mp4")
+#import the class  from mediapipe
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
 
 while True:
       success, img = cap.read()
-
-      cv2.imshow("Hand", img)
+      imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+      results = hands.process(imgRGB)
+      print(results)
+      
+      
+      cv2.imshow("Hand", results)
       cv2.waitKey(1)
+      
 
 
 
